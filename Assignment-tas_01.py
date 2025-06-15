0138844030d82483e8905a15851dff0e91da5855
Mo Alam <akhan398635@gmail.com>
2025-06-15 20:03:31 +0530

    added about

---
+++ Assignment-tas_01.py
@@ -0,0 +1,24 @@
+#Task- 1
+
+a = int(input('Enter the first number: '))
+b = int(input('Enter the second number: '))
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+print('Addition' , c)
+print('Subtraction' , d)
+print('Multiplication' , e)
+print('Division' , f)
+
+'''
+OUTPUT ------>
+
+Enter the first number: 12
+Enter the second number: 6
+Addition 18
+Subtraction 6
+Multiplication 72
+Division 2.0
+
+'''
---
+++ Assignment_task_02.py
@@ -0,0 +1,16 @@
+#Task - 2
+
+a =input('Enter your fisrt name : ')
+b =input('Enter your second name : ')
+a=str(a)
+b=str(b)
+c = a + b
+print ('HEllo! welcome ', c )
+
+'''
+Output 
+
+Enter your fisrt name : alam
+Enter your second name : bhai
+HEllo! welcome  alambhai
+'''
